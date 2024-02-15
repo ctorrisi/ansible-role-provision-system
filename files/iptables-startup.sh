0509817db54ad2614ecc4de1eb329a844e9b1fdb
Corey Torrisi <3690732+ctorrisi@users.noreply.github.com>
2024-02-15 23:18:15 +1100

    fix: iptables

--- files/iptables-startup.sh
+++ files/iptables-startup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sleep 10
-
 LAN_CIDR=$(ip route show default 2>/dev/null | awk '/default via/ {print $3}' | awk -F'.' '{print $1"."$2".0.0/16"}')
 
 iptables -I INPUT 2 -s ${LAN_CIDR} -j ACCEPT
