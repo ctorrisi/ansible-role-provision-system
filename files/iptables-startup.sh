2ce4d743c86d630f181e3e8ac0f04178f960e61e
Corey Torrisi <3690732+ctorrisi@users.noreply.github.com>
2024-03-04 14:39:38 +1100

    chore: remove tasks related to dynamic iptables

--- files/iptables-startup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-LAN_CIDR=$(ip route show default 2>/dev/null | awk '/default via/ {print $3}' | awk -F'.' '{print $1"."$2".0.0/16"}')
-
-iptables -I INPUT 2 -s ${LAN_CIDR} -j ACCEPT
